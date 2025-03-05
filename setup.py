5aaa3e75b062014c58459e644d085d99cb07d000
Codebulous <josephemmanuelchay@gmail.com>
2025-03-05 08:09:10 +0800

    Chore: updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="linguify",
     version="0.1",
     packages=find_packages(),
-    install_requires=[],
+    package_data={
+        # Include any non-Python files that should be part of the package
+        "linguify": ["*.md"],
+    },
+    install_requires=[
+
+    ],
+    description="Because conversations should resonate.",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/josephchay/linguify",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12.7",
 )
