4a63ebc4f3307c61f69e0ef44080efa2541f2959
Codebulous <josephemmanuelchay@gmail.com>
2025-03-06 16:25:55 +0800

    feat(deps,setup): added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="linguify",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="Because conversations should resonate.",
+)
